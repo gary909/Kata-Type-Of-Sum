c594d523349a6bb21b57f1bbedfa30160359c10a
gary909 <garywhite61@hotmail.com>
2022-10-17 12:52:25 +0200

    code

---
+++ app.js
@@ -0,0 +1,9 @@
+function typeOfSum(a, b) {
+    // good luck
+}
+
+console.log(typeOfSum(12, 1)); // 'number'
+console.log(typeOfSum('d', 1)); // 'string'
+console.log(typeOfSum(1, 'a')); // 'string'
+console.log(typeOfSum('dd', '')); // 'string'
+console.log(typeOfSum(null, false)); // 'number'
